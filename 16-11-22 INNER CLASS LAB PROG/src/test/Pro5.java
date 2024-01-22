dbca48a49551f24904dd4c001d3f8190bfad84d8
santosh <gunasantosh4@gmail.com>
2024-01-22 23:27:44 +0530

    Changes the Class

--- 16-11-22 INNER CLASS LAB PROG/src/test/Pro5.java
+++ 16-11-22 INNER CLASS LAB PROG/src/test/Pro5.java
@@ -1,18 +1,15 @@
 package test;
 
-class Foo
-{
-   class Bar
-   {
-	   
-   }	
+class Foo {
+	class Bar {
+
+	}
 }
-public class Pro5 
-{
-	public static void main(String[] args) 
-	{
-         Foo f=new Foo();
-                Foo.Bar ob=f.new Bar();
+
+public class Pro5 {
+	public static void main(String[] args) {
+		Foo f = new Foo();
+		Foo.Bar ob = f.new Bar();
 	}
 
 }
