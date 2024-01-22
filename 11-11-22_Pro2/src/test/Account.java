bb7f910bceca0180a96f0838d9a8c079b3144832
santosh <gunasantosh4@gmail.com>
2024-01-22 23:23:45 +0530

    Changes the Class

--- 11-11-22_Pro2/src/test/Account.java
+++ 11-11-22_Pro2/src/test/Account.java
@@ -1,22 +1,23 @@
 package test;
 
-public class Account
-{
-    long accNo;
-    double bal;
-    
-    
+public class Account {
+	long accNo;
+	double bal;
+
 	public long getAccNo() {
 		return accNo;
 	}
+
 	public void setAccNo(long accNo) {
 		this.accNo = accNo;
 	}
+
 	public double getBal() {
 		return bal;
 	}
+
 	public void setBal(double bal) {
 		this.bal = bal;
 	}
-    
+
 }
