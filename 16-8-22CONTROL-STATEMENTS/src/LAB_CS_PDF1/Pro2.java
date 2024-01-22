680f336fd89c20294a5eaf2e11f87cc8639e837f
santosh <gunasantosh4@gmail.com>
2024-01-22 23:29:51 +0530

    Changes on lab's codes the Class

--- 16-8-22CONTROL-STATEMENTS/src/LAB_CS_PDF1/Pro2.java
+++ 16-8-22CONTROL-STATEMENTS/src/LAB_CS_PDF1/Pro2.java
@@ -10,28 +10,26 @@ If any of the given numbers are zero, return -2.
 If the given numbers are positive, return the greatest.*/
 package LAB_CS_PDF1;
 
-public class Pro2
-{
-	public int  getGreatest(int a,int b)
-	{
-		if(a<0||b<0)
+public class Pro2 {
+	public int getGreatest(int a, int b) {
+		if (a < 0 || b < 0)
 			return -1;
-		
-		else if(a==0||b==0)
+
+		else if (a == 0 || b == 0)
 			return 0;
-		
+
 		else {
-			if(a>b)
+			if (a > b)
 				return a;
 			else
 				return b;
-		     }
-		
-			//System.out.println("greatest");
+		}
+
+		// System.out.println("greatest");
 	}
-	   public static void main(String[] args)
-	   {
-		Pro2 aobj=new Pro2();
-		System.out.println(aobj.getGreatest(5,9));
-	   }
+
+	public static void main(String[] args) {
+		Pro2 aobj = new Pro2();
+		System.out.println(aobj.getGreatest(5, 9));
+	}
 }
