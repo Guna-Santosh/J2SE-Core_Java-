faea2dbda547015f6230fa7eadc6d6b51a72a99f
santosh <gunasantosh4@gmail.com>
2024-01-22 23:20:47 +0530

    Changes the Class

--- 09-10-22 INNER CLASS STATIC/src/test/Boss.java
+++ 09-10-22 INNER CLASS STATIC/src/test/Boss.java
@@ -1,14 +1,12 @@
 package test;
 
-public class Boss 
- {
-	  public static void main(String[] args) 
-	  {
-	    OuterClass oc=new OuterClass();
-	    oc.m1();
-	    OuterClass.InnerClass ic=new OuterClass.InnerClass();
-    ic.m1();
-    ic.m11();
-  }
-	  
+public class Boss {
+	public static void main(String[] args) {
+		OuterClass oc = new OuterClass();
+		oc.m1();
+		OuterClass.InnerClass ic = new OuterClass.InnerClass();
+		ic.m1();
+		ic.m11();
+	}
+
 }
