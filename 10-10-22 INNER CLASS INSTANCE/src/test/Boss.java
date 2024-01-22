cba9c615eabbaa23f1a5043452561731768fe361
santosh <gunasantosh4@gmail.com>
2024-01-22 23:21:38 +0530

    Changes the Class

--- 10-10-22 INNER CLASS INSTANCE/src/test/Boss.java
+++ 10-10-22 INNER CLASS INSTANCE/src/test/Boss.java
@@ -1,15 +1,14 @@
 package test;
-public class Boss 
-{
-	public static void main(String[] args) 
-	{
-   	   OuterClass oc=new OuterClass();
-   	   oc.m1();
-   	   OuterClass.m11();//(or) oc.m1();
-   	   OuterClass.InnerCLass ic=oc.new InnerCLass();
-   	   //
-   	   ic.m22();
-   	   ic.m2();
+
+public class Boss {
+	public static void main(String[] args) {
+		OuterClass oc = new OuterClass();
+		oc.m1();
+		OuterClass.m11();// (or) oc.m1();
+		OuterClass.InnerCLass ic = oc.new InnerCLass();
+		//
+		ic.m22();
+		ic.m2();
 	}
 
 }
--- 10-10-22 INNER CLASS INSTANCE/src/test/OuterClass.java
+++ 10-10-22 INNER CLASS INSTANCE/src/test/OuterClass.java
@@ -1,31 +1,31 @@
 package test;
-public class OuterClass 
-{
-    int a=10;
-    static int b=20;
-    void m1() 
-    {  int c=20;
-	  System.out.println(a);
-	  System.out.println(b);
-    }//end of nsm()
-    static void m11() 
-    {
-	  //System.out.println(a);
-	  System.out.println(b);
-	  
-    }//end of sm()
+
+public class OuterClass {
+	int a = 10;
+	static int b = 20;
+
+	void m1() {
+		int c = 20;
+		System.out.println(a);
+		System.out.println(b);
+	}// end of nsm()
+
+	static void m11() {
+		// System.out.println(a);
+		System.out.println(b);
+
+	}// end of sm()
 //-----------------------------------------------------------    
-  public class InnerCLass
-  {
-      void m2() 
-      {
-    	  System.out.println(a);
-    	  System.out.println(b);
-      }
-      static void m22() 
-      {
-    	  //System.out.println(a);
-    	  System.out.println(b);
-      }
-  }
+
+	public class InnerCLass {
+		void m2() {
+			System.out.println(a);
+			System.out.println(b);
+		}
+
+		static void m22() {
+			// System.out.println(a);
+			System.out.println(b);
+		}
+	}
 }
