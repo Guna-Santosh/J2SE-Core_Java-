2a026fd29261c107500ae592e80bc95009eed5b3
santosh <gunasantosh4@gmail.com>
2024-01-22 23:25:59 +0530

    Changes the Class

--- 16-11-22 INNER CLASS LAB PROG/src/test/Pro1.java
+++ 16-11-22 INNER CLASS LAB PROG/src/test/Pro1.java
@@ -1,43 +1,25 @@
 package test;
 
-class A
-{
- class b extends A
- {
-	 
-	interface C
-	{
-		interface D extends C
-		{
-			
-			
+class A {
+	class b extends A {
+
+		interface C {
+			interface D extends C {
+
+			}
 		}
-	} 
-	 class E implements test.A.b.C.D
-	 {
-		 
-	 }
-	 
- }	
+
+		class E implements test.A.b.C.D {
+
+		}
+
+	}
 }
 
+public class Pro1 {
 
+	public static void main(String[] args) {
 
-
-
-
-
-
-
-
-public class Pro1
-{
-     
-	
-	public static void main(String[] args) 
-	{
-	
-		
 	}
 
 }
