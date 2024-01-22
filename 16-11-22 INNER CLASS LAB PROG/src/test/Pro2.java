6bae95e7c73cb5f061e048c79acdb530f99c5c04
santosh <gunasantosh4@gmail.com>
2024-01-22 23:26:23 +0530

    Changes the Class

--- 16-11-22 INNER CLASS LAB PROG/src/test/Pro2.java
+++ 16-11-22 INNER CLASS LAB PROG/src/test/Pro2.java
@@ -1,37 +1,38 @@
 package test;
 
-class Boo
-{
-	Boo(String s)
-	{
-		
-	}	
-	Boo(){}
+class Boo {
+	Boo(String s) {
+
+	}
+
+	Boo() {
+	}
 }
-class Bar extends Boo
-{
-   Bar(){}
-	Bar(String s)
-	{
+
+class Bar extends Boo {
+	Bar() {
+	}
+
+	Bar(String s) {
 		super(s);
 	}
-	void zoo() 
-	{
-		Boo f=new Bar() {};
-		Boo f1=new Bar("hi") {};
-		
-		//Boo f=new Boo(24) {};
-		//Bar f=new Boo.Bar(String s) {};
-		//Bar f=new Boo(String s) {};
+
+	void zoo() {
+		Boo f = new Bar() {
+		};
+		Boo f1 = new Bar("hi") {
+		};
+
+		// Boo f=new Boo(24) {};
+		// Bar f=new Boo.Bar(String s) {};
+		// Bar f=new Boo(String s) {};
 	}
-	
+
 }
 
-public class Pro2 
-{
+public class Pro2 {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 
 	}
 
